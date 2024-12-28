810216658dd67bdc3ccbc336c2d419d5caea8077
JW <jihaneessakhi02@gmail.com>
2024-12-29 06:41:23 +0800

    changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Contact from "./pages/Contact";
 
 function App() {
   return (
-    <Router basename="/myportfolio">
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
