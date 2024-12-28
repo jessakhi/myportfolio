a47fccac4860f1ef8c8fbf259545d98f15f88588
JW <jihaneessakhi02@gmail.com>
2024-12-29 06:59:57 +0800

    changes

--- src/App.js
+++ src/App.js
@@ -4,16 +4,22 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Portfolio from "./pages/Portfolio";
 import Contact from "./pages/Contact";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <Router basename="/myportfolio">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/portfolio" element={<Portfolio />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+      <Navbar />
+      <div style={{ minHeight: "80vh" }}> {/* Ensures space between Navbar and Footer */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+      <Footer />
     </Router>
   );
 }
