1111ff9a89b08f276f2554542d7b1fa0c89255a9
JW <jihaneessakhi02@gmail.com>
2024-12-29 07:10:55 +0800

    changes

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Footer from "./components/Footer";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/myportfolio">
       <Navbar />
       <div style={{ minHeight: "80vh" }}> {/* Ensures space between Navbar and Footer */}
         <Routes>
